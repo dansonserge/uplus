cff72800d5b278afe6f399f88b72e673e1fbea38
Clement <muhirwaclement@gmail.com>
2018-03-21 16:50:46 +0200

    Update index.php

--- api/index.php
+++ api/index.php
@@ -907,8 +907,8 @@
 					 		"recipients"	=>$recipients,
 					 		"message"		=>$message,
 					 	);
-				 	//include 'sms.php';
-				 	//if($httpcode == 200){}
+				 	include 'sms.php';
+				 	if($httpcode == 200){}
 
 					// Bwiuld the answel
 					$returnedinformation    = array();   
