18cd67c19e704723479e2632f8c9674db33ff70c
Hirwa Jean Luc <hirwajluc@gmail.com>
2018-04-06 15:50:10 +0200

    fb

--- api/invest.php
+++ api/invest.php
@@ -53,7 +53,7 @@
 		require('db.php');
 		$memberId	= mysqli_real_escape_string($db, $_POST['memberId']);
 		$forumId	= mysqli_real_escape_string($db, $_POST['forumId']);
-		$query 		= $investDb->query("INSERT INTO forumUser (forumCode, userCode) VALUES ('$forumId','$memberId','$memberId')")or die(mysqli_error($investDb));
+		$query 		= $investDb->query("INSERT INTO forumUser (forumCode, userCode, createdBy) VALUES ('$forumId','$memberId','$memberId')")or die(mysqli_error($investDb));
 		echo "Done";
 	}
 
