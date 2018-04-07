09e50692c1e38857fe14b2f65d735f22ebe181a7
Clement <muhirwaclement@gmail.com>
2018-04-07 10:51:35 +0200

    Update invest.php

--- api/invest.php
+++ api/invest.php
@@ -24,11 +24,11 @@
 	{
 		require('db.php');
 		$memberId		= mysqli_real_escape_string($db, $_POST['memberId']);
-		$query = $investDb->query("SELECT id forumId, title, subtitle, IFNULL((SELECT mine FROM forummember WHERE memberId = '$memberId'),'YES') AS mine  FROM forums WHERE archive <> 'YES' OR isnull(archive)")or die(mysqli_error($investDb));
+		$query = $investDb->query("SELECT id forumId, title, subtitle, IFNULL((SELECT mine FROM forummember WHERE memberId = '$memberId'),'YES') AS mine  FROM forums WHERE archive <> 'YES'")or die(mysqli_error($investDb));
 		$forums = array();
 		while ($forum = mysqli_fetch_array($query))
 		{
-			if($forum['mine'] == 'YES')
+			if(!$forum['mine'] == 'NO')
 			{
 				$joined = '0';
 			}
