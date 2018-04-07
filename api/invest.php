1c4567d057dc302c35d6e3c1743825941f574a12
Clement <muhirwaclement@gmail.com>
2018-04-07 12:22:23 +0200

    Update invest.php

--- api/invest.php
+++ api/invest.php
@@ -1,4 +1,28 @@
-require('db.php');
+<?php
+// START INITIATE
+	include ("db.php");
+	// var_dump($_SERVER["REQUEST_METHOD"]);
+	if ($_SERVER["REQUEST_METHOD"] == "POST") 
+	{
+		if(isset($_POST['action']))
+		{
+			$_POST['action']();
+		}
+		else
+		{
+			echo 'Please read the API documentation';
+		}
+	}
+	else
+	{
+		echo 'UPLUS API V02';
+	}
+// END INITIATE
+
+// START FORUMS
+	function listForums()
+	{
+		require('db.php');
 		$memberId		= mysqli_real_escape_string($db, $_POST['memberId']);
 		$query = $investDb->query("SELECT id forumId, title, subtitle, IFNULL((SELECT mine FROM forummember WHERE memberId = '$memberId'),'YES') AS mine  FROM forums WHERE archive <> 'YES'")or die(mysqli_error($investDb));
 		$forums = array();
@@ -22,3 +46,32 @@ require('db.php');
 		header('Content-Type: application/json');
 		$forums = json_encode($forums);
 		echo $forums;
+	}
+
+	function joinForum()
+	{
+		require('db.php');
+		$memberId	= mysqli_real_escape_string($db, $_POST['memberId']);
+		$forumId	= mysqli_real_escape_string($db, $_POST['forumId']);
+		if(mysqli_num_rows($investDb->query("SELECT * FROM forumuser WHERE forumCode = '$forumId' AND userCode = '$memberId'"))>0)
+		{
+
+		}
+		else
+		{
+			$query 		= $investDb->query("INSERT INTO forumuser (forumCode, userCode, createdBy) VALUES ('$forumId','$memberId','$memberId')")or die(mysqli_error($investDb));
+		}
+		echo "Done";
+	}
+
+	function exitForum()
+	{
+		require('db.php');
+		$memberId	= mysqli_real_escape_string($db, $_POST['memberId']);
+		$forumId	= mysqli_real_escape_string($db, $_POST['forumId']);
+		$query 		= $investDb->query("UPDATE forumuser SET archive = 'YES' WHERE forumCode = '$forumId' and userCode = '$memberId')")or die(mysqli_error($investDb));
+		echo "Done";
+	}
+
+// END FORUMS
+?>
