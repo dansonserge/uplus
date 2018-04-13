09e467c5c118e7f6e954e8d31885f30b3f1406d6
wswapped <wswapped@gmail.com>
2018-04-13 10:32:07 +0200

    remove shortcuts

--- churches/menu-header.php
+++ churches/menu-header.php
@@ -19,41 +19,19 @@
                             <div class="uk-grid uk-dropdown-grid">
                                 <div class="uk-width-2-3">
                                     <div class="uk-grid uk-grid-width-medium-1-3 uk-margin-bottom uk-text-center">
-                                        <a href="page_mailbox.html" class="uk-margin-top">
+                                        <a href="broadcast.php" class="uk-margin-top">
                                             <i class="material-icons md-36 md-color-light-green-600">&#xE158;</i>
-                                            <span class="uk-text-muted uk-display-block">Mailbox</span>
+                                            <span class="uk-text-muted uk-display-block">Broadcast</span>
                                         </a>
-                                        <a href="page_invoices.html" class="uk-margin-top">
+                                        <a href="invoices.php" class="uk-margin-top">
                                             <i class="material-icons md-36 md-color-purple-600">&#xE53E;</i>
                                             <span class="uk-text-muted uk-display-block">Invoices</span>
                                         </a>
-                                        <a href="page_chat.html" class="uk-margin-top">
+                                        <a href="feeds.php" class="uk-margin-top">
                                             <i class="material-icons md-36 md-color-cyan-600">&#xE0B9;</i>
-                                            <span class="uk-text-muted uk-display-block">Chat</span>
-                                        </a>
-                                        <a href="page_scrum_board.html" class="uk-margin-top">
-                                            <i class="material-icons md-36 md-color-red-600">&#xE85C;</i>
-                                            <span class="uk-text-muted uk-display-block">Scrum Board</span>
-                                        </a>
-                                        <a href="page_snippets.html" class="uk-margin-top">
-                                            <i class="material-icons md-36 md-color-blue-600">&#xE86F;</i>
-                                            <span class="uk-text-muted uk-display-block">Snippets</span>
-                                        </a>
-                                        <a href="page_user_profile.html" class="uk-margin-top">
-                                            <i class="material-icons md-36 md-color-orange-600">&#xE87C;</i>
-                                            <span class="uk-text-muted uk-display-block">User profile</span>
+                                            <span class="uk-text-muted uk-display-block">Feeds</span>
                                         </a>
                                     </div>
-                                </div>
-                                <div class="uk-width-1-3">
-                                    <ul class="uk-nav uk-nav-dropdown uk-panel">
-                                        <li class="uk-nav-header">Components</li>
-                                        <li><a href="components_accordion.html">Accordions</a></li>
-                                        <li><a href="components_buttons.html">Buttons</a></li>
-                                        <li><a href="components_notifications.html">Notifications</a></li>
-                                        <li><a href="components_sortable.html">Sortable</a></li>
-                                        <li><a href="components_tabs.html">Tabs</a></li>
-                                    </ul>
                                 </div>
                             </div>
                         </div>
