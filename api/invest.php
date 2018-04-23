5e526496f0047fd524ba13e88472110cf7fd9e32
wswapped <wswapped@gmail.com>
2018-04-23 23:14:29 +0200

    s

--- api/invest.php
+++ api/invest.php
@@ -113,7 +113,7 @@
 		// $sql = $investDb->query("SELECT F.id feedId, F.feedForumId, (SELECT COUNT(*) FROM feed_likes WHERE feedCode = F.id) as nlikes, (SELECT COUNT(*) FROM feed_likes WHERE feedCode = F.id AND userCode = '$memberId') as liked, (SELECT COUNT(*) FROM feed_comments  WHERE feedCode = F.id) as comments, F.feedTitle, U.name feedBy, U.userImage feedByImg, F.createdDate feedDate,F.feedContent FROM investments.feeds F INNER JOIN uplus.users U ON F.createdBy = U.id")or die(mysqli_error($investDb));
 		$feeds = array();
 
-		for ($n=0; $n<count($all_feeds) && $n<2; $n++)
+		for ($n=0; $n<count($all_feeds); $n++)
 		{
 			$row = $all_feeds[$n];
 			//liked status of the user
