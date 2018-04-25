39772d85a6c82a56e43adeeac4f8b5aea3de0f23
wswapped <wswapped@gmail.com>
2018-04-25 21:36:57 +0200

    s

--- api/invest.php
+++ api/invest.php
@@ -254,10 +254,10 @@
 	        		//looping through image
 	        		foreach ($attachments as $key => $value) {
 		        		$filename = "invest/gallery/feeds/";
-					    $image_parts = explode(";base64,", $value);
-					    $image_type_aux = explode("image/", $image_parts[0]);
+					    // $image_parts = explode(";base64,", $value);
+					    // $image_type_aux = explode("image/", $image_parts[0]);
 					    // $image_type = $image_type_aux[1];
-					    $image_base64 = base64_decode($image_parts[1]);
+					    $image_base64 = base64_decode($value);
 					    $file = $filename . uniqid() . '.png';
 					    file_put_contents("../".$file, $image_base64);
 
