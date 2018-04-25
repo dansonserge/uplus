bda27459a4f4b17181b8ae53f409fa82e129598f
wswapped <wswapped@gmail.com>
2018-04-25 21:32:17 +0200

    sd

--- api/invest.php
+++ api/invest.php
@@ -256,7 +256,7 @@
 		        		$filename = "invest/gallery/feeds/";
 					    $image_parts = explode(";base64,", $value);
 					    $image_type_aux = explode("image/", $image_parts[0]);
-					    $image_type = $image_type_aux[1];
+					    // $image_type = $image_type_aux[1];
 					    $image_base64 = base64_decode($image_parts[1]);
 					    $file = $filename . uniqid() . '.png';
 					    file_put_contents("../".$file, $image_base64);
