325de134b934093ad51a428daf49dcb9f0a098f1
wswapped <wswapped@gmail.com>
2018-05-01 18:35:30 +0200

    s

--- churches/feeds.php
+++ churches/feeds.php
@@ -544,7 +544,7 @@
 
 				ajax.addEventListener("load", function(){
 					setTimeout(function(){
-						location.reload()
+						// location.reload()
 					}, 1500)                    
 				})
 			}else{
